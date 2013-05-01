26e797956ab70129b70d6f6e86314c9cf03c45b8
Tomohiro Komiya <komiya@lifestyle-design.jp>
2013-05-01 19:16:05 +0900

    修正

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo "diff";
 	$str_browser_language = !empty($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? strtok(strip_tags($_SERVER['HTTP_ACCEPT_LANGUAGE']), ',') : '';
 	$str_browser_language = !empty($_GET['language']) ? $_GET['language'] : $str_browser_language;
 	switch (substr($str_browser_language, 0,2))
